db2658795d2f687847d46f180fa82b97716d9dde
luis <senaluissilva883@gmail.com>
2026-09-24 20:55:08 -0500

    Add UsuarioAdmin configuration and user list tests

--- Usuarios/admin.py
+++ Usuarios/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import Usuario
+
+
+@admin.register(Usuario)
+class UsuarioAdmin(UserAdmin):
+    model = Usuario
+    list_display = ('email', 'first_name', 'last_name', 'rol', 'estado_cuenta', 'is_staff')
+    list_filter = ('rol', 'estado_cuenta', 'is_staff', 'is_superuser')
+    search_fields = ('email', 'first_name', 'last_name', 'documento')
+    ordering = ('email',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Información personal', {'fields': ('first_name', 'last_name', 'documento', 'telefono')}),
+        ('Rol y estado', {'fields': ('rol', 'estado_cuenta')}),
+        ('Permisos', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Fechas importantes', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'rol', 'password1', 'password2'),
+        }),
+    )
 
 # Register your models here.
